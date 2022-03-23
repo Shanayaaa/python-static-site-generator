d39e3068018cde7e32d088f446c60c9dc070cabf
Shanayaaa <72222137+Shanayaaa@users.noreply.github.com>
2022-03-23 20:55:47 +0000

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -2,4 +2,7 @@ from typing import List
 from pathlib import Path
 
 class Parser:
-    extensions = []
+    extensions : List[str] = []
+
+    def valid_extension(self, extension):
+        return extension in self.extension
