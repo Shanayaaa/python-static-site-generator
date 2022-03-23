2bb80e5db853345f3637ae1fbd2ffb82da4cf951
Shanayaaa <72222137+Shanayaaa@users.noreply.github.com>
2022-03-23 14:30:21 +0000

    Update ssg.py

--- ssg.py
+++ ssg.py
@@ -1,12 +1,14 @@
 from importlib import import_module
 
 
-import_module('typer')
+import typer
 from ssg.site import Site
 
 def main(source = "content", dest = "dist"):
     config = {
-        'source' : 'source',
-        'dest' : 'dest'
+        'source' : source,
+        'dest' : dest
     }
-    
+    Site(**config).build()
+
+typer.run(main)
